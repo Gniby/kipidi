99d59de5d8e1f483f2c58dfe2b5f90f8361cd6a5
pushparaj <rajcspalani@gmail.com>
2017-08-18 00:20:29 +0530

    angular upgraded and bower.json included

--- public/app/controller/controllers.js
+++ public/app/controller/controllers.js
@@ -3,10 +3,11 @@ var controllers = angular.module('myapp.controllers', []);
 controllers.controller('IndexController', ['$scope','$http', function($scope,$http) {
     
 	$scope.message = "hello";
+	$scope.users = [];
 	$http.get('/user').then(function(res){
-		$scope.userList = res.data;
-		for(var i = 0; i < $scope.userList.length;i++){
-			console.log($scope.userList[i]);
+		$scope.users = res.data;
+		for(var i = 0; i < $scope.users.length;i++){
+			console.log($scope.users[i]);
 		}
 	},function(err){
 		console.log('error');
